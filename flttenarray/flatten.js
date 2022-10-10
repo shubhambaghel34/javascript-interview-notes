5cc3ce60724e82ec5d442e660d73e08a9e775262
Shubham_Baghel <shubhambaghel34@gmail.com>
2022-10-10 13:08:19 +0000

    flatten any array

---
+++ flttenarray/flatten.js
@@ -0,0 +1,15 @@
+const flatten = function(arr, result = []) {
+  let length = arr.length;
+  for (let i = 0; i < length; i++) {
+    const value = arr[i];
+    if (Array.isArray(value)) {
+      flatten(value, result);
+    } else {
+      result.push(value);
+    }
+  }
+  return result;
+};
+//flatten any array
+
+console.log(flatten([[[1, [1.1]], 2, 3], [4, 5]]))
