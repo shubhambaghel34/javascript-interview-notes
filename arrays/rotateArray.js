0a3b340c08bb5317ff61eeb4ac00e54eddcc0931
Shubham Baghel <shubhambaghel34@gmail.com>
2025-03-28 11:42:42 +0530

    rotate array code

--- arrays/rotateArray.js
+++ arrays/rotateArray.js
@@ -10,15 +10,36 @@ function rotateArray(arr){
     return arr;
 }
 
-console.log(rotateArray(randomArray));
+//console.log(rotateArray(randomArray));
 
 
 
 
-// function roatateArrayByPlaces(nums,d){
-// temp= [1,2,3]
+function roatateArrayByPlaces(arr, d) {
+    let n = arr.length;
 
+    // Handle the case where d > size of array
+    d %= n;
 
+    // Reverse the first d elements
+    reverse(arr, 0, d - 1);
 
-// }
-// console.log(roatateArrayByPlaces(randomArray,3));
+    // Reverse the remaining n-d elements
+    reverse(arr, d, n - 1);
+
+    // Reverse the entire array
+    reverse(arr, 0, n - 1);
+}
+
+// Function to reverse a portion of the array
+function reverse(arr, start, end) {
+    while (start < end) {
+        let temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+console.log(roatateArrayByPlaces(randomArray,3));
